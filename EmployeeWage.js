a4b326fb191bc2e7fb2e9203e13dcd35d11444a4
VivekGujale <vdgujale007@.com>
2022-12-27 15:53:44 +0530

    Added logic to calculate emp wage by using function getworkHrs

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -8,19 +8,17 @@ const WAGE_PER_HOUR = 20
 let empHrs = 0;
 let empCheck = Math.floor(Math.random() * 10) % 3
 console.log(empCheck);
-switch (empCheck) {
+function getWorkHrs(empCheck) {
 
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HOURS;
-        break;
-
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOURS;
-        break;
-
-    default:
-        empHrs = 0;
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
 }
-
+empHrs = getWorkHrs(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Employee Wage = " + empWage);
