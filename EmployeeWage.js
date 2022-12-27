9fba2c5b6c738542f5a36be62f2d2d9d3b33c0bb
VivekGujale <vdgujale007@.com>
2022-12-27 16:09:13 +0530

    Added logic to calculate monthly emp wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -4,10 +4,9 @@ const IS_FULL_TIME = 2
 const PART_TIME_HOURS = 4
 const FULL_TIME_HOURS = 8
 const WAGE_PER_HOUR = 20
-
+const NUM_OF_WORKING_DAYS = 10
 let empHrs = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3
-console.log(empCheck);
+
 function getWorkHrs(empCheck) {
 
     switch (empCheck) {
@@ -19,6 +18,12 @@ function getWorkHrs(empCheck) {
             return 0;
     }
 }
-empHrs = getWorkHrs(empCheck);
+
+for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+    empCheck = Math.floor(Math.random() * 10) % 3;
+    console.log(empCheck);
+    empHrs += getWorkHrs(empCheck);
+}
+
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Employee Wage = " + empWage);
+console.log("Total Working Hours = " + empHrs + "\nEmployee Wage = " + empWage);
