567863e23c19fa34f3fa1d880603fe10d70157fc
VivekGujale <vdgujale007@.com>
2022-12-27 16:22:34 +0530

    Added logic to calculate emp wages till condition

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -5,7 +5,11 @@ const PART_TIME_HOURS = 4
 const FULL_TIME_HOURS = 8
 const WAGE_PER_HOUR = 20
 const NUM_OF_WORKING_DAYS = 10
-let empHrs = 0;
+const MAX_HOURS_IN_MONTH = 100
+
+let empHrs = 0
+let totalEmpHrs = 0
+let totalWorkingDays = 0
 
 function getWorkHrs(empCheck) {
 
@@ -19,11 +23,13 @@ function getWorkHrs(empCheck) {
     }
 }
 
-for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+while (totalEmpHrs <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
     empCheck = Math.floor(Math.random() * 10) % 3;
     console.log(empCheck);
-    empHrs += getWorkHrs(empCheck);
+    totalEmpHrs += getWorkHrs(empCheck);
 }
 
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total Working Hours = " + empHrs + "\nEmployee Wage = " + empWage);
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("\nTotal Working Days = " + totalWorkingDays + "\nTotal Working Hours = " + totalEmpHrs +
+    "\nEmployee Wage = " + empWage);
