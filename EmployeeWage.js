bfb2792c7c9427a0a641837e1eac365245fd2a9a
VivekGujale <vdgujale007@.com>
2022-12-27 15:28:25 +0530

    added logic to check Employee is present or absent

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,8 @@
 console.log("Welcome to Employee wage")
+const IS_ABSENT = 0
+let employeeCheck = Math.floor(Math.random() * 10) % 2
+console.log(employeeCheck);
+if (employeeCheck == IS_ABSENT)
+    console.log("Employee is Absent")
+else
+    console.log("Employee is Present")
